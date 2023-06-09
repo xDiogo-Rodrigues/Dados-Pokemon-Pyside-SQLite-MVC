f5b759e0f259b527fd918867c86769ae249af4a9
xDiogo-Rodrigues <diogosouzak11@gmail.com>
2023-06-09 01:34:20 -0300

    Adicionado alguns widgets, a tela inicial pronta

--- views/mainwindow.py
+++ views/mainwindow.py
@@ -4,7 +4,30 @@ from PySide6.QtCore import QSize, Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
+        """metodo que vai representa a tela principal da aplicação"""
         super().__init__()
+        self.setStyleSheet('background-color: #00BFFF;')
         self.setWindowTitle('POKEMON')
-        self.setWindowIcon(QPixmap('views/images/icon_pokebola.png'))
-        self.setFixedSize(QSize(500,400))
+        self.setWindowIcon(QPixmap('views/images/icon_pokemon.png'))
+        self.setFixedSize(QSize(320,230))
+
+        #widgets
+        self.lbl_imagem_pokemon = QLabel()
+        self.lbl_imagem_pokemon.setPixmap(QPixmap('views/images/mobile_icon.png'))
+        self.lbl_imagem_pokemon.setAlignment(Qt.AlignVCenter | Qt.AlignHCenter)
+        self.lbl_nome_pesuisa = QLabel('DIGITE O NOME DO POKEMON PARA SER PESQUISADO')
+        self.line_edit_nome_pokemon = QLineEdit()
+        self.line_edit_nome_pokemon.setAlignment(Qt.AlignVCenter | Qt.AlignHCenter)
+        self.button = QPushButton('PESQUISAR')
+       
+        #layout
+        layout = QFormLayout()
+        layout.addWidget(self.lbl_imagem_pokemon)
+        layout.addWidget(self.lbl_nome_pesuisa)
+        layout.addWidget(self.line_edit_nome_pokemon)
+        layout.addWidget(self.button)
+
+        #container
+        container = QFrame()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
